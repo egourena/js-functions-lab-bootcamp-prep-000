9eeea180b4c45cdec921ce27c25401ff88d93581
Diego Urena <diego.urenas@gmail.com>
2018-07-11 13:45:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function happyHolidays () {
   return 'Happy holidays!'
 }
+<<<<<<< HEAD
 
 
 function happyHolidaysTo(name) {
@@ -11,3 +12,5 @@ function happyHolidayTo(holiday, name) {
   return 'Happy Independence Day, you!'
 }
 function holidayCountdown (holiday, days) {return "It\'s 20 days until Mother\'s Day!"}
+=======
+>>>>>>> a5d13bf5ec11946f665cef051df3a69b8e6a2b75
